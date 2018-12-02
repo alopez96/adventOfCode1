80e4de05729caee6d52ab2434868077f2b6a8e38
Arturo <alopez96@ucsc.edu>
2018-12-01 21:24:59 -0800

    part 1 complete

--- final-freq.js
+++ final-freq.js
@@ -7,8 +7,32 @@ function finalfreq() {
   if(err) {
     console.log('error', err);
   }
-  
+  //get all strings
+  const strings = data.toString();
+
+  //break up the strings
+  const stringsArray = strings.split('\n');
+
+  //testing
+  console.log('array', stringsArray);
+  console.log('total steps', stringsArray.length);
+
+
+  var intArray = []
+  var total = 0;
+  var integer = 0;
+  //loop through frequencies
+  for(i = 0; i < stringsArray.length; i++){
+    intArray.push(stringsArray[i])
+  }
+
+  for(i = 0; i < stringsArray.length; i++){ 
+    integer = Number(stringsArray[i]);
+    total += integer
+  }
+
   console.timeEnd('executiontime');
+  console.log('total', total);
 })
 
 }
