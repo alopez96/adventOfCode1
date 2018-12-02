037b0ba09382f5bcab1773a4dcccc1c154704029
Arturo <alopez96@ucsc.edu>
2018-12-01 22:13:26 -0800

    bug in loop

--- final-freq.js
+++ final-freq.js
@@ -17,10 +17,11 @@ function finalfreq() {
   console.log('array', stringsArray);
   console.log('total steps', stringsArray.length);
 
-
   var intArray = []
   var total = 0;
   var integer = 0;
+  var integer2 = 0;
+
   //loop through frequencies
   for(i = 0; i < stringsArray.length; i++){
     intArray.push(stringsArray[i])
@@ -31,8 +32,28 @@ function finalfreq() {
     total += integer
   }
 
+  var j = 1;
+  var i = 0;
+  var total2 = 0;
+  var firstfreq = stringsArray[0];
+  var newArray = [0];
+  do{
+    integer2 = Number(stringsArray[i]);
+    total2 += integer2
+    newArray[j] = total2
+    i++
+    if(i = (stringsArray.length-1)){
+      i = 0
+    }
+    j++
+    console.log('testing', newArray)
+  }while(!newArray.includes(total2))
+
+
   console.timeEnd('executiontime');
   console.log('total', total);
+  console.log('firstfreq', firstfreq);
+  
 })
 
 }
